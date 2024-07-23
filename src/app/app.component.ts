746ddeb61e2c694afd63385348891e5cbd6b8b74
James Lampkin <jamesmlampkin@outlook.com>
2024-07-23 05:07:50 -0500

    Added angular front end.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { SiteHeaderComponent } from "./site-header/site-header.component";
+import { SiteFooterComponent } from "./site-footer/site-footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, SiteHeaderComponent, SiteFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
